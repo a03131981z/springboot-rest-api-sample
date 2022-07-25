a0429d2c9ce533ff27d81f789a1486b79a9d2b0f
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-07-25 10:50:58 -0300

    Criando end-point de listar todos os usuários

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +44,12 @@ public class GreetingsController {
     	usuarioRepository.save(usuario);/*Grava no banco de dados*/
     	
     	return "Olá Mundo "+nome;
+    }
+    
+    @GetMapping(value = "listatodos")/*Nosso primeiro método de API*/
+    @ResponseBody /*retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();/*Executa a consulta no banco de dados*/
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/*retorna a lista em JSON*/
     }
 }
