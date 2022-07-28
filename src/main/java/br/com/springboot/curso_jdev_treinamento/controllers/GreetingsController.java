2d4d82647ed1eab01a499178e87844627cd59391
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-07-28 15:16:22 -0300

    Criando end-point para consultar por parte do nome

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -89,4 +89,11 @@ public class GreetingsController {
     	Usuario usuario = usuarioRepository.findById(iduser).get();
     	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
     }
+    
+    @GetMapping(value = "buscarpornome") //Mapeia a url
+    @ResponseBody //Descrição da resposta
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name){
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+    }
 }
